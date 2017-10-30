df680beb8ff6fb70bb99fe731b2b9dfefe7de259
AidanRB <aidanrichbennett+github@gmail.com>
2017-10-30 00:14:43 -0400

    In-progress getall.py

---
+++ getall.py
@@ -0,0 +1,24 @@
+import aoapi
+from bs4 import BeautifulSoup
+
+aoapi.username = "arb"
+aoapi.password = "Don't short-@mb13!"
+print(aoapi.gatherCredentials())
+
+def getSubfs(forumhtml):
+    soup = BeautifulSoup(str(forumhtml), "html.parser")
+    htmls = []
+    for item in soup.find_all("li"):
+        if(item.find_all("strong") == []):
+            htmls.append(item)
+    nums = []
+    for item in htmls:
+        soup = BeautifulSoup(str(item))
+        a = soup.find("a")
+        link = a["href"]
+        nums.append(link[58:-5])
+    return nums
+
+subnums = getSubfs(str(aoapi.getForum(21,1)[3][0]))
+
+print(aoapi.getForum(subnums[0],1))
